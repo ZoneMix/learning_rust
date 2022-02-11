98d2a9fbd1ad3dfeda4a812854c15cfc37219512
ZoneMix <bailey.belisario@trojans.dsu.edu>
2022-02-11 10:40:13 -0600

    Added cross compilation in grep

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -1,15 +1,35 @@
 use std::fs::File;
-#[cfg(unix)]
-use std::os::unix::io::{AsRawFd, FromRawFd};
 use std::io::Read;
 
+#[cfg(unix)]
+use std::os::unix::io::{AsRawFd, FromRawFd};
+
+#[cfg(windows)]
+use { 
+    kernel32::GetStdHandle,
+    std::os::windows::io::{AsRawHandle, FromRawHandle, RawHandle},
+    winapi::*,
+};
 
 fn main() {
-    if !cfg!(target_os = "windows") {
-        let mut f: File = unsafe { File::from_raw_fd(0) };
-    }
+    #[cfg(unix)]
+    let mut f: File = get_linux_stdin();
+
+    #[cfg(windows)]
+    let mut f: File = get_win_stdin();
+
     let mut input = String::new();
     f.read_to_string(&mut input).unwrap();
-
     println!("I read: \n{}", input);
+}
+
+#[cfg(windows)]
+fn get_win_stdin() -> File {
+    let handle: RawHandle = unsafe { GetStdHandle(winapi::winbase::STD_INPUT_HANDLE) };
+    unsafe { File::from_raw_handle(handle) }
+}
+
+#[cfg(unix)]
+fn get_linux_stdin() -> File {
+    unsafe { File::from_raw_fd(0) }
 }
